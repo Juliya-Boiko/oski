83b0594dbf8757653011e05f981c8828a7aeaadf
Juliya Boiko <boikojuliya2707@gmail.com>
2023-12-14 13:21:32 +0200

    update patchUserScore

--- backend/controllers/user/patchUserScore.js
+++ backend/controllers/user/patchUserScore.js
@@ -1,41 +1,31 @@
 import User from '../../models/User.js';
 import Answer from '../../models/Answer.js';
-import { strict as assert } from 'node:assert';
 
 export const patchUserScore = async (req, res) => {
   // GET VALUES FROM REQUEST
   const { _id: userId } = req.user;
   const { quizId, values } = req.body;
+
+  const { marks } = await User.findById(userId);
+  const { answers: correct } = await Answer.findOne({ quizId });
+
+  // CALCULATE SCORE
   const formattedValues = Object.entries(values).map((el) => {
-    return { [el[0]]: el[1] }
+    return { answerId: el[0], answer: el[1] }
   });
+  const formattedAnswers = correct.reduce((acc, v) => ({ ...acc, [v.answerId]: v.answer}), {});
+  const score = formattedValues.reduce((ac, el) => {
+    return el.answer === formattedAnswers[el.answerId] ? ac + 1 : ac
+  }, 0);
 
-  const { answers } = await Answer.findOne({ quizId });
-  
+  // UPDATE USER 
+  const passedQuizIdx = marks.findIndex(el => el.quizId.toString() === quizId.toString());
+  marks[passedQuizIdx].score = score;
+  const updatedUser = await User.findByIdAndUpdate(userId, { marks }, { new: true });
+
+  // GENERATE RESPONSE
   res.status(201).json({
-    formattedValues, answers
+    updatedUser
   });
-  // }
-  // 
-  // const correct = answers.reduce((acc, v) => ({ ...acc, [v.answerId]: v.answer}), {});
-  
-  // const { quizId, score } = req.body;
-
-  // const { marks } = await User.findById(userId);
-  // const idx = marks.findIndex((el) => el.quizId.toString() === quizId.toString());
-  // if (idx === -1) {
-  //   res.status(404).json({
-  //     message: 'Not found quiz'
-  //   });
-  // } else {
-  //   // UPDATE SCORE
-  //   marks[idx].score = score;
-  //   await User.findByIdAndUpdate(userId, { marks }, { new: true });
-
-  //   // GENERATE RESPONSE
-  //   res.status(201).json({
-  //     message: 'Updated successfully',
-  //   });
-  // }
 };
 
